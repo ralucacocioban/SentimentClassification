1ac0fdfb47202c6e64912d53f684a6e2d47310e7
Minh-Long Do <minhlong94@yahoo.de>
2015-04-03 13:48:29 +0100

    Cleaned up into a tool with commandline input. Able to load word2vec model.

--- TopicExtraction.py
+++ TopicExtraction.py
@@ -5,6 +5,7 @@ from Politweet import *
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from gensim.models import Word2Vec
+from argparse import ArgumentParser
 
 stemmer = SnowballStemmer("english")
 
@@ -15,14 +16,57 @@ def remove_stopwords(tokens):
     no_links = [token for token in temp if token != "http" or token not in regex.findall(token)]
     return no_links
 
+# Get the file from this link: https://code.google.com/p/word2vec/
+def load_word2vec_model(c_bin):
+    if not os.path.isfile(c_bin):
+        sys.exit("""File '{0}' does not exist.""".format(c_bin))
+
+    model = Word2Vec.load_word2vec_format(c_bin, binary=True)
+
+    return model
+
+def input_check(tweets, word2vec):
+    if not os.path.isfile(tweets):
+        print """File path for tweets '{0}' does not exist.""".format(tweets)
+        return False
+
+    if not os.path.isfile(word2vec):
+        print """File path for word2vec model '{0}' does not exist.""".format(word2vec)
+        return False
+
+    return True
+
+
+def main():
+    """
+    Instruction how to use this tool.
+    python TopicExtraction.py --tweets [tweet dataset path] --word2vec [word2vec model path]
+    """
+    
+    parser = ArgumentParser(description='Extract topics from tweeter dataset.')
+    parser.add_argument('--tweets', '-t', help='File path of the tweets dataset.', required=True)
+    parser.add_argument('--word2vec', '-w2v', help='File path of the word2vec model.', required=True)
+    args = parser.parse_args()
+
+    print "Tweet file location:", args.tweets
+    print "Word2Vec file location:", args.word2vec
+
+    if not input_check(args.tweets, args.word2vec):
+        sys.exit(-1)
+
+    print "Preprocessing tweets..."
+    tweets = pre_processing(get_tweets(args.tweets))
+    print "Done preprocessing tweets."
+
+    print "Removing stopwords, punctuations etc."
+    tweets["clean"] = tweets["tokens"].apply(remove_stopwords)
+    print "Done removing stopwords, punctuations etc."
+
+    print "Loading word2vec model..."
+    word2vec_model = load_word2vec_model(args.word2vec)
+    print "Done loading word2vec model."
+
+
 
 if __name__ == "__main__":
-    filename = "debate08_sentiment_tweets.tsv"
-    if not os.path.isfile(filename):
-        sys.exit("Missing file.")
-
-    print """Extracting tweets from file '{}'""".format(filename)
-    print "Display tweets"
-    tweets = pre_processing(get_tweets(filename))
-    tweets["clean"] = tweets["tokens"].apply(remove_stopwords)
-    print tweets["clean"]
+    main()
