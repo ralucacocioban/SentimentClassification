4581467008535125ff1c89529bfb37e44292fc60
Minh-Long Do <minhlong94@yahoo.de>
2015-04-12 00:38:24 +0100

    Add function to read relevant topic file.

--- tweet_topic_extr.py
+++ tweet_topic_extr.py
@@ -4,7 +4,11 @@ import nltk
 import math
 import sentiWordNet
 import Politweet
+import os
+import sys
+import codecs
 from Politweet import get_tweets
+from match_tweet_to_topic import load_word2vec_model, similarity,
 
 
 def extractTopics(tweet):
@@ -15,9 +19,20 @@ def extractTopics(tweet):
 		if(elem['pos'] in ['NN', 'NNS', 'NNP', 'NNPS']):
 			topics.append(elem['lemma']);
 
-	# print tweet;		
-	# print topics;		
-	return topics;	
+	# print tweet;
+	# print topics;
+	return topics;
+
+def read_relevant_topics(fname):
+	if not os.path.isfile(fname):
+		sys.exit("File does not exist.")
+	relevant_topics = []
+
+	with codecs.open(fname, 'r', 'utf-8') as f:
+		for line in f:
+			word = line.strip()
+			relevant_topics.append(word)
+	return relevant_topics
 
 def filterNouns(tweet_topics):
 	for tweet in tweet_topics:
@@ -34,7 +49,5 @@ if __name__ == '__main__':
 		topics = extractTopics(t['content'])
 		if(topics != []):
 			tweet_topics.append({'tweet': t['content'], 'topics' : topics});
-	
-	filterNouns(tweet_topics)	
 
-
+	filterNouns(tweet_topics)
